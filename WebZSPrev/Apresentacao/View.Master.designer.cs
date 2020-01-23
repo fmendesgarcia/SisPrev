9c270def5ccb3bb3171da25010f399ce6f130812
fmendesgarcia <fm.wonka@gmail.com>
2020-01-23 18:02:06 -0300

    first upload

---
+++ WebZSPrev/Apresentacao/View.Master.designer.cs
@@ -0,0 +1,53 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace WebZSPrev.Apresentacao
+{
+
+
+    public partial class View
+    {
+
+        /// <summary>
+        /// Controle head.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
+
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Controle menu_server.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl menu_server;
+
+        /// <summary>
+        /// Controle ContentPlaceHolder1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+    }
+}
---
+++ WebZSPrev/ObjetoTransferencia/Processo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebZSPrev.ObjetoTransferencia
+{
+    public class Processo
+    {
+        public int id { get; set; }
+        public string numero_processo { get; set; }
+        public int MyProperty { get; set; }
+
+
+        // id_cliente
+        // Numero do Processo, ex: 5025545-79.2019.4.03.6182
+        // Status do processo, ex: Demandante
+        // Área de atuação, ex: Direito cível
+        // Objeto da ação, ex: Embargos de terceiro
+        // Assunto
+        // Data contratação
+        // Data encerramento
+        // Data distribuição
+        // vara
+        // comarca
+        // fase, ex: instrução
+        // valor da causa
+    }
+}
